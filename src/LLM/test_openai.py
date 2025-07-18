6238c379a7f0277c53c81caea5c226f632ffb5b1
HDB <debinhan@qq.com>
2025-07-18 02:39:52 +0000

    拉取远端代码后，重新预处理数据

--- src/LLM/test_openai.py
+++ src/LLM/test_openai.py
@@ -1,4 +1,9 @@
 from openai import OpenAI
+import os
+
+os.environ["http_proxy"]="http://10.61.2.90:1082"
+os.environ["https_proxy"]="http://10.61.2.90:1082"
+
 client = OpenAI()
 
 response = client.responses.create(
