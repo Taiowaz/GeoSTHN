4a13a5b87f439fdd14e20d310edf6daca4e3e920
HDB <debinhan@qq.com>
2025-07-21 11:30:05 +0000

    1. train.py与test.py文件合并为train_test.py文件 2. 实验目录重构，去除以时间戳为实验文件夹的一部分 3. 去除generate_short_dir_name函数

--- src/main.py
+++ src/main.py
@@ -14,9 +14,8 @@ from tgb.utils.utils import set_random_seed, save_results
 from utils.config import get_args
 from utils.load_model import load_model
 from utils.load_data import load_all_data
-from utils.test import test
+from src.train_test import test
 from utils.log import setup_logger
-from utils.utils import generate_short_dir_name
 
 # 定义主函数
 def main(args):
@@ -28,19 +27,17 @@ def main(args):
     exp_name = args.exper_name
     # 存放实验相关文件
     exper_dir = os.path.join(args.exper_base_dir, exp_name, args.dataset)
-    args_str = generate_short_dir_name(args)
-    exper_args_dir = os.path.join(exper_dir, args_str)
-    checkpoint_dir = os.path.join(exper_args_dir, "checkpoint")
-    result_dir = os.path.join(exper_args_dir, "result")
+    checkpoint_dir = os.path.join(exper_dir, "checkpoint")
+    result_dir = os.path.join(exper_dir, "result")
     result_filename = f"{result_dir}/{args.model}_{args.dataset}_results.json"
     # 创建目录
     os.makedirs(exper_dir, exist_ok=True)
-    os.makedirs(exper_args_dir, exist_ok=True)
+    os.makedirs(exper_dir, exist_ok=True)
     os.makedirs(checkpoint_dir, exist_ok=True)
     os.makedirs(result_dir, exist_ok=True)
     # 2. 设置日志记录器
     # ---------------
-    log_file = f"{exper_args_dir}/{exp_name}.log"
+    log_file = f"{exper_dir}/{exp_name}.log"
     setup_logger(log_file)
     logging.info(f"开始实验: {exp_name}")
     logging.info(f"配置信息: {args}")
@@ -180,8 +177,8 @@ def main(args):
                 "data": args.dataset,
                 "run": run_idx,
                 "seed": args.seed,
-                f"val {metric}": f"{perf_metrics_val_mean: .4f} ± {perf_metrics_val_std: .4f}",
-                f"test {metric}": f"{perf_metrics_test_mean: .4f} ± {perf_metrics_test_std: .4f}",
+                f"val {metric}": f"{perf_metrics_val_mean: .4f}  +- {perf_metrics_val_std: .4f}",
+                f"test {metric}": f"{perf_metrics_test_mean: .4f} +- {perf_metrics_test_std: .4f}",
                 "test_time": test_time,
                 "tot_train_val_time": val_time,
             },
